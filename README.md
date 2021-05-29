536420ea786652c4576d5c57bebae7a6646b3916
Ivan <ivanchernetskey@mail.ru>
2021-05-29 17:52:18 +0300

    use insertone for insetr object in mongodb

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Install MongoDB Community Edition
 1.Create db&collections also drop collection
 
 ![img.png](img.png)
+
+2.insertOne
+![img_1.png](img_1.png)
--- mongo.js
+++ mongo.js
@@ -1,2 +1,17 @@
 //здесь я буду писать команды которые будут вставлять в cmd для разбора того как они работатют 
 
+//добаляем js объект в DB
+db.users.insertOne(
+    {
+        "name":"Tom",
+        "email":"test@mail.ru",
+        "age":19,
+        "hasCar":true,
+        "favColors":["Зеленый", "Красный", "Желтый"],
+        "child": {
+            "name": "Jack",
+            "surname": "Rich",
+            "age": 5
+        }
+    }
+)
